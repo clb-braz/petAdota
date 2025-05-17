0ee7e8c9913aae7b53cbede4140d2869db1a2ed7
clb-braz <clb.braz@protonmail.com>
2025-05-17 15:37:40 -0300

    Create PetAdotaApplication.java

---
+++ PetAdotaApplication.java
@@ -0,0 +1,11 @@
+package com.petadota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdotaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PetAdotaApplication.class, args);
+    }
+}
